5b3e04e509fe2c66ab06f2739b11787d15145473
PureKoor <38826280+PureKoor@users.noreply.github.com>
2020-11-22 05:11:19 -0500

    Update tf.js
    fixing line 5

--- twitch/tf.js
+++ twitch/tf.js
@@ -2,7 +2,6 @@ tx=[`Gooey`,`BIG`,`ˢᵐᵒˡ`,`Cute`,`Rubber`,`Fluffy`,`Buff`,`Chubby`,``,``,``
 ra=[`slowly`,`meticulously`,`casually`,`sluggishly`,`rapidly`,`swiftly`];
 tf=[`Bear`,`Cat`,`Chicken`,`Cow`,`Crab`,`Croc`,`Deer`,`Dog`,`Dragon`,`Goat`,`Giraffe`,`Horse`,`Ott`,`Owl`,`Raccoon`,`Raven`,`Shark`,`Tiger`,`Tanuki`,`Wolf`,`Ox`,`Tycoon`]; 
 
-"me now " + ra[Math.floor(Math.random()*ra.length)] + " transforms " + m +" into a " + tx[Math.floor(Math.random()*tx.length)] + " " + tf[Math.floor(Math.random()*tf.length)];
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
 }
