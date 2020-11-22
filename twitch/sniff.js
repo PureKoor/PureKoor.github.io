c868bab0eb64ea45c80cb8916ba79fd214774d67
PureKoor <38826280+PureKoor@users.noreply.github.com>
2020-11-22 18:52:29 -0500

    Update sniff.js
    updated words

--- twitch/sniff.js
+++ twitch/sniff.js
@@ -1,6 +1,30 @@
-smell = [`Flowery`,`musk`,`antiseptic`,`comfort`,`heaviness`,`coldness`,`warmness`,`heat`,`an intoxicating nature`,`coffee`,`earth`,`burntness`,`pureness`
-,`impureness`,`ketchup`,`onions`,`strawberries`,`heaven`,`water`,`flowers`,`mint`,`wood`,`game`,`fish`,`suishi`,`citrus`,`apple`,`tiger`,
-`raccoon`,`furry drama`,`vtuber`,`popufur`,`twitter dot com`,`twitch dot com`,`wealth`,`smells`,`AHHH`,`gamer`];
+smell = [`an intoxicating nature`,`like, comment, & subscribe. Hit that bell for notifications...`
+         `aloe`,`antiseptic`,`apple`,`AHHH`,
+         `bondfire`,`burntness`,`butter toast`,`banana`,
+         `comfort`,`coldness`,`coffee`,`citrus`,`caramel`,`cat`,
+         `dog`,
+         `egg`,`earth`,
+         `fire`,`flowers`,`fish`,`furry drama`,`furry website`,`furries`,
+         `gaming`,`game`,`gamer`,
+         `horseradish`,`heaviness`,`heat`,`heaven`,
+         `ice`,`impureness`,
+         `jollyness`,
+         `kale`,`ketchup`,`kobold`,
+         `loading`,`lavender`,
+         `mustard`,`musk`,`mint`,
+         `nugget`,`newness`,
+         `oldness`,`onions`,`oranges`,
+         `paw`,`pureness`,`popufur`,
+         `questing`,
+         `roses`,`raccoon`,`rain`,`rubber`,
+         `strawberries`,`suishi`,`smells`,`streaming`,`sour apple`,
+         `tea`,`tiger`,`twitter dot com`,`twitch dot com`,`tycoon`,
+         `uranium`,
+         `volcano`,`vtuber`,`vanilla`,
+         `water`,`warmness`,`water`,`wood`,`wealth`,`watermelon`,
+         `X-mas`,
+         `yellow`,
+         `zinc`];
 
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
