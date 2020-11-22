e1287c8f92cecc9502e6276ede5e7cfe9bb5594c
PureKoor <38826280+PureKoor@users.noreply.github.com>
2020-11-22 05:25:01 -0500

    Update transformation.js
    test

--- twitch/transformation.js
+++ twitch/transformation.js
@@ -1,6 +1,6 @@
 tx=[`Gooey`,`BIG`,`ˢᵐᵒˡ`,`Cute`,`Rubber`,`Fluffy`,`Buff`,`Chubby`,``,``,``,``];
 ra=[`slowly`,`meticulously`,`casually`,`sluggishly`,`rapidly`,`swiftly`];
-tf=[`Bear`,`Cat`,`Chicken`,`Cow`,`Crab`,`Croc`,`Deer`,`Dog`,`Dragon`,`Goat`,`Giraffe`,`Horse`,`Ott`,`Owl`,`Raccoon`,`Raven`,`Shark`,`Tiger`,`Tanuki`,`Wolf`,`Ox`,`Tycoon`]; 
+tf=[`a`,`b`,`c`,`d`,`e`,`f`,`g`,`h`,`i`,`j`,`k`,`l`,`m`,`n`,`o`,`p`,`q`,`r`,`s`,`t`,`u`,`v`,`w`,`x`,`y`,`z`,`1`,`2`,`3`,`4`,`5`,`6`,`7`,`8`,`9`,`10`,`11`,`12`,`13`,`14`,`15`,`16`,`17`,`Bear`,`Cat`,`Chicken`,`Cow`,`Crab`,`Croc`,`Deer`,`Dog`,`Dragon`,`Goat`,`Giraffe`,`Horse`,`Ott`,`Owl`,`Raccoon`,`Raven`,`Shark`,`Tiger`,`Tanuki`,`Wolf`,`Ox`,`Tycoon`]; 
 
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
