e8f46b5693de9f2d4e002cc31658d40c7ba842e1
PureKoor <38826280+PureKoor@users.noreply.github.com>
2020-11-22 06:41:57 -0500

    Update and rename puritylevel to puritylevel.js
    updated

--- twitch/puritylevel.js
+++ twitch/puritylevel.js
@@ -1,16 +1,14 @@
 vip=["Koor is the Purest Tycoon","Zasz is the Purest Cute Crocco"];
 users={"purekoor": vip[0],"pure koor": vip[0],"koor": vip[0], "zasz": vip[1],"zaszthecroc": vip[1]};
-anon=[`is amazingly pure`,`is as pure as the ocean breeze`,`is as pure as a freshly baked donut`,`as pure as a chicken pot pie`,`is as pure as ketchup!!!`,`is very pure!`,`is pure!`,`has a good amount of purity`,`is somewhat pure?`,`could be pure`,`could be more pure`,`could be impure`,`could be less impure`,
+anon=[`is amazingly pure`,`is as pure as the ocean breeze`,`is as pure as a freshly baked donut`,`as pure as a chicken pot pie`,`is as pure as ketchup!!!`,
+`is very pure!`,`is pure!`,`has a good amount of purity`,`is somewhat pure?`,`could be pure`,`could be more pure`,`could be impure`,`could be less impure`,
  `is ehhh... pure?`,`is somewhat impure...`,`is impure`,`...way too impure`,`off the charts impure`,`is as impure as a smol kobold`,`is as impure as a bawt`];
  
- 
- 
- function choose(arr){
+function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)];
 }
 
-function puritylevel(user) {
-  
+function puritylevel(user) {  
   name = user.toLowerCase();
   
   if (name in users) {
