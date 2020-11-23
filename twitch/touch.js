7d372425511d5cf55b5f2290851802f9024ef539
PureKoor <38826280+PureKoor@users.noreply.github.com>
2020-11-22 19:05:54 -0500

    Update touch.js
    Updated words

--- twitch/touch.js
+++ twitch/touch.js
@@ -1,6 +1,31 @@
-poke=[`gooey`,`fuzzy`,`velvety`,`silky`,`slippery`,`smooth`,`a musky husky`,`cool`,`impure`,`pure`,`icy`,`leathery`,
-      `moist`,`mushy`,`squeaky`,`fluffy`,`...uh...`,`wooly`,`sensitive`,`prickly`,`firm`,`boiling`,`soggy`,`dusty`,`wooly`];
 amt=[`slightly`,`very`,`kinda`,`somewhat`,`...in a way...`,`largely`,`deeply`,`viscerally`,`firmly`,``,``,``,``];
+poke=[`like a musky husky`,`...uh...`,`...Error: Scan could not detect texture`,
+      `ashy`,
+      `bumpy`,`boiling`,
+      `cute`,`cozy`,`cool`,
+      `dehydrated`,`dusty`,
+      `elastic`,
+      `feathery`,`fuzzy`,`fluffy`,`firm`,
+      `glossy`,`grooved`,`gooey`,
+      `hoarse`,
+      `icy`,`impure`,
+      `jagged`,
+      `knitted`,
+      `level`,`leathery`,
+      `mosaic`,`moist`,`mushy`,
+      `nice`,
+      `orderly`,
+      `polished`,`pure`,
+      `quiet`,
+      `refinded`,
+      `scaled`,`silky`,`slippery`,`smooth`,`squeaky`,`sensitive`,`prickly`,`soggy`,
+      `thorny`,
+      `uncompormising`,
+      `varnished`,`velvety`,
+      `wavy`,`wooly`,
+      `yielding`,
+      `zigg-zaggy`];
+
 
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
