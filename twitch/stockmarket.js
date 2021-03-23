b58c37267f8fe19acffa2620dca9177e9c1372e3
PureKoor <38826280+PureKoor@users.noreply.github.com>
2021-03-22 23:56:54 -0400

    Update stockmarket.js
    updated descriptions for cap bad

--- twitch/stockmarket.js
+++ twitch/stockmarket.js
@@ -1,14 +1,21 @@
-stocksNames[`MAW`,`PAW`,`BEANS`,`FLUFF`,`FLOOF`,`TAIL`,`HORN`,`YOU`.`Your stocks`];
+stocksNames[`MAW`,`PAW`,`BEANS`,`FLUFF`,`FLOOF`,`TAIL`,`HORN`,`YOU`.`Your personal stock`];
 wordchanges=[`slightly`,`very`,`kinda`,`somewhat`,`...in a vwey vig way...`,`largely`];
 Size=[`Large`];
-whyCapBad[`We have enough food production to feed every yet don't`,`We've built temp outdor resturants for resturants owners faster than we've built temp houses for the houseless`,`1% of the wealthy have as much money as the bottom 50%, or else said 100 peps have as much as x`,`We can transport goods in less than 2 days but can't feed the x going hungry`,`video game industry`,`we ca make golden donuts but not x`,`medcine`];
+whyCapBad[`Without better distribution systems or local food production for the goal of feeding everyone, over producing food on farms can lead to destroying food to keep market prices higher or to avoid high distribution costs. This becomes more apparent during certain times such as the pandemic, where more people were starving https://www.nytimes.com/2020/04/11/business/coronavirus-destroying-food.html`
+          `Money is actually just paper we say has value. It's hard to carry around paper though, so one day we'll probably just have points in an account to spend. Like in a videogame!!!`,
+          `There are more vaccant homes then there are houseless people. https://www.self.inc/info/empty-homes/ There are many ways to end houseless-ness, the easiest way is to give people a house, such as what Findland did along with other tactics https://www.huffpost.com/entry/homelessness-finland-housing-first_n_5c503844e4b0f43e410ad8b6`
+          `Wealth Inequality in the USA between the rich and everyone else is massive. Such hoarding of wealth by the few restricts how much money is useable by everyone https://en.wikipedia.org/wiki/Wealth_inequality_in_the_United_States`,
+          `We can transport goods in less than 2 days via mail, shoot robots through space to land on a moving planet in space, and in 2018 around 11.1% of USA households were food insecure! https://en.wikipedia.org/wiki/Hunger_in_the_United_States`,
+          `In 2019, a videogame company's CEO made $40 million dollars as some employees couldn't afford food https://www.businessinsider.com/activision-blizzard-salary-disparity-issues-2020-8`,
+          `Around the world some people starve... BUT WE CAN PRODUCE GOLDEN DONUTS!!! Worth it? https://spoonuniversity.com/how-to/where-to-get-cheese-in-edinburgh`,
+          `Here in the USA there are around 100,000 people who die due to not receiving needed healthcare https://en.wikipedia.org/wiki/Health_insurance_coverage_in_the_United_States`];
 
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
 }
 
 function inflation(){
-  var numberOfOptions = 2;
+  var numberOfOptions = 3;
   [Math.floor(Math.random()*numberOfOptions]
   
    it a bull market (?), x has/have deflated by %/.x is basically y now
