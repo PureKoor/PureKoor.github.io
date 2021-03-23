31b2d9970341d050557a9e06e63cf59f495cdaa0
PureKoor <38826280+PureKoor@users.noreply.github.com>
2021-03-23 00:40:43 -0400

    Update stockmarket.js
    updated overall code

--- twitch/stockmarket.js
+++ twitch/stockmarket.js
@@ -1,6 +1,6 @@
 stocksNames[`MAW`,`PAW`,`BEANS`,`FLUFF`,`FLOOF`,`TAIL`,`HORN`,`YOU`.`Your personal stock`];
 wordchanges=[`slightly`,`very`,`kinda`,`somewhat`,`...in a vwey vig way...`,`largely`];
-Size=[`Large`];
+Size=[`Large`,`Greatly, making you big and round`];
 whyCapBad[`Without better distribution systems or local food production for the goal of feeding everyone, over producing food on farms can lead to destroying food to keep market prices higher or to avoid high distribution costs. This becomes more apparent during certain times such as the pandemic, where more people were starving https://www.nytimes.com/2020/04/11/business/coronavirus-destroying-food.html`
           `Money is actually just paper we say has value. It's hard to carry around paper though, so one day we'll probably just have points in an account to spend. Like in a videogame!!!`,
           `There are more vaccant homes then there are houseless people. https://www.self.inc/info/empty-homes/ There are many ways to end houseless-ness, the easiest way is to give people a house, such as what Findland did along with other tactics https://www.huffpost.com/entry/homelessness-finland-housing-first_n_5c503844e4b0f43e410ad8b6`
@@ -10,35 +10,90 @@ whyCapBad[`Without better distribution systems or local food production for the 
           `Around the world some people starve... BUT WE CAN PRODUCE GOLDEN DONUTS!!! Worth it? https://spoonuniversity.com/how-to/where-to-get-cheese-in-edinburgh`,
           `Here in the USA there are around 100,000 people who die due to not receiving needed healthcare https://en.wikipedia.org/wiki/Health_insurance_coverage_in_the_United_States`];
 
+options = {
+    capbad:8,
+    bull:50,
+    bear:30,
+    stag:12,
+};
+
 function choose(arr){
-  return arr[Math.floor(Math.random()*arr.length)] + " ";
+  return arr[Math.floor(Math.random()*arr.length)] + " get rid of maybe ";
 }
 
-function inflation(){
-  var numberOfOptions = 3;
-  [Math.floor(Math.random()*numberOfOptions]
-  
-   it a bull market (?), x has/have deflated by %/.x is basically y now
-
+function findMarket(input) {
+    arr = [];
+    for(options in input) {
+        if ( input.hasOwnProperty(options) ) {
+            for( i=0; i<input[options]; i++ ) {
+                arr.push(options);
+            }
+        }
+    }
+    return arr[Math.floor(Math.random() * arr.length)];
 }
 
-function deflation(){
-    it a bear market (?), x has/have inf;ated by %/.x is basically a y now
+function capitalismBad(){
+  message = "You're outside the stock market. An anti-capitalist next to you whispers, " ";
+  message += choose(whyCapBad);
+  message += " "";
+          
+  return message; 
 }
 
-function stagnation(){
-   The market stays steady the same. you are still x.
+function bullMarket(){
+  message = "You're in a bear market! ";
+  message += choose(stockNames);
+  message += " has gone x ";
+  message += choose(wordChanges);
+  message += "Your porfolio has x ";
+  message += choose(size);
+          
+          
+  return message; 
 }
 
-function captialismBad(){
-  Outside the stock market: x
+function bearMarket(){
+  message = "You're in a bear market! ";
+  message += choose(stockNames);
+  message += " has gone x ";
+  message += choose(wordChanges);
+  message += "Your porfolio has x ";
+  message += choose(size);
+          
+  message += "it a bull market (?), x has/have deflated by %/ x is basically y now";
+  message += "The market stays steady the same. you are still x."
+          
+  return message; 
 }
 
-function touch(user) {
-  percentage
-  word
-  percent chance for each type
-  
-  return choose(arr);
+function stagMarket(){
+  message = "You're in a bear market! ";
+  message += choose(stockNames);
+  message += " has gone x ";
+  message += choose(wordChanges);
+  message += "Your porfolio has x ";
+  message += choose(size);     
+          
+  return message; 
 }
+
+
+function stockmarket(user){
+  outcome = findMarket(options);        
+
+  if(outcome == "bull"){
+         return capitalismBad();
+  }    
+  if(outcome == "bear"){
+         return capitalismBad();
+  }    
+  if(outcome == "stag"){
+         return capitalismBad();
+  }    
+  if(outcome == "capbad"){
+         return capitalismBad();
+  }    
+}
+   
 stockmarket(arg);
