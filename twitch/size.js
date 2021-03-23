151989f77fbaf691dae956fbd32d343061dda59d
PureKoor <38826280+PureKoor@users.noreply.github.com>
2021-03-22 22:50:42 -0400

    Update size.js
    updated descriptions values

--- twitch/size.js
+++ twitch/size.js
@@ -1,6 +1,6 @@
-size = ["micro","macro","general human height","tiny","lorgest","bggest","ˢᵐᵒˡ"];
-sizeSize =["12ft","11ft","10ft","9ft","8ft","7ft","6ft","5ft","4ft","3ft","2ft","1ft","smol as a rat","ˢᵐᵒˡ as a rat","big as a donut"];
-parts = ["general size","paw","maw","tail","height","wallet","food","shirt","pants","shoes","hair","height","computer","phone"];
+size = ["micro","macro","general human height","tiny","lorgest","ˢᵐᵒˡ","huge","small","little","an undefined size"];
+sizeSize =["12ft","11ft","10ft","9ft","8ft","7ft","6ft","5ft","4ft","3ft","2ft","1ft","smol as a rat","ˢᵐᵒˡ as a rat","big as a donut","biggest","largest"];
+parts = ["general size","paw","maw","tail","height","wallet","food","shirt","pants","shoes","hair","height","computer","phone","trinket","hair","purity"];
 
 function choose(arr){
   return arr[Math.floor(Math.random()*arr.length)] + " ";
