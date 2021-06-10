e1d9ecc8bd01c437216ad74354bb7cf59095a20f
PureKoor <38826280+PureKoor@users.noreply.github.com>
2021-06-10 18:05:08 -0400

    Create hug.js

---
+++ twitch/hug.js
@@ -0,0 +1,43 @@
+ra=[`walks`,`runs`,`teleports`,`rolls`,`slides`,`swiftly runs`,`poofs`,`quickly turns`];
+
+ts=[`Big`,
+   `Smol`,
+   ` `,` `,` `,` `];
+
+tx=[`Bear`
+    ,`Ghost`
+    ,`Lightly`,
+    `Friend`,`Side`,`Furry`,
+    ` `,` `,` `,` `,` `];
+
+tf=[`hugs`,`double hugs`,`tripple hugs`];
+
+
+function choose(arr){
+  return arr[Math.floor(Math.random()*arr.length)] + " ";
+}
+
+function hug(user) {
+ hug_combine = choose(ts);
+ hug_combine += choose(tx);
+ hug_combine += choose(tf);
+ hug_result = hug_combine.trim(); 
+
+ which_A = "";
+ x = hug_result.charAt(0).toUpperCase();
+ if (x == 'A' || x ==  'E' || x ==  'I' || x ==  'O' || x ==  'U') {
+    which_A = " an ";
+  } else {
+    which_A = " a ";
+  }
+    
+  str = "/me " + user + " ";
+  str += choose(ra);
+  str += "to give " + touser;
+  str += which_A;
+  str += "🤗 ";
+  str += hug_result;
+
+  return str;
+}
+hug(arg);
