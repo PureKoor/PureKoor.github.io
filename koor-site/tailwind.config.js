af5b8b9dee0767cabc7548151fe540b9e4993477
Zasz <44956770+bluezasz@users.noreply.github.com>
2024-06-06 03:30:56 -0700

    Website Revamp + deploy

---
+++ koor-site/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      serif: ["Fugaz One", "ui-serif", "serif"],
+      sans: ["Roboto", "system-ui", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
