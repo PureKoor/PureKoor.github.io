bb4642a6dd3bbece5d29bc8c1875c52b08c42160
PureKoor <38826280+PureKoor@users.noreply.github.com>
2025-05-25 01:00:05 -0700

    Update Deploy Site

---
+++ build/twitch/rpgGameSymbols.js
@@ -0,0 +1,15 @@
+//For one to copy into other files if need be
+
+const GAME_EMOJI = {
+  class: "💼",
+  hp: "❤️",
+  atk: "⚔️",
+  int: "🎓",
+  def: "🛡️",
+  spd: "💨",
+  spd: "☘️",
+  power: "🌋",
+  wisdom: "🌟",
+  agility: "👟",
+  prayer: "✝️",
+};
