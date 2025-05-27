208d286dbf0eff56e72f783cfb1b33c5564f0edc
Zasz <44956770+bluezasz@users.noreply.github.com>
2025-05-26 18:31:10 -0700

    Use react-player for youtube video

--- koor-site/src/components/Boxes/Main_Youtube.js
+++ koor-site/src/components/Boxes/Main_Youtube.js
@@ -1,36 +1,29 @@
-import Ad_YoutubePlayer from "../KoorFunction/Ad_YoutubePlayer";
+import ReactPlayer from "react-player/lazy";
 import Func_AutoScrollThumbnails from "../KoorFunction/Func_YT-ThumbnailScroll";
+import { FaYoutube } from "react-icons/fa6";
 
 function Main_Youtube() {
   return (
     <div
       id="youtube"
-      className="bg-[#ffb600] max-w-screen-xl flex flex-row justify-between h-full items-center gap-5 flex-wrap lg:flex-nowrap w-full"
+      className="bg-[#ffb600] max-w-screen-xl flex flex-row justify-between items-center flex-wrap lg:flex-nowrap w-full"
     >
-      <div className="rounded-xl border-4 border-white m-3 overflow-hidden shrink-0  mx-auto">
-        <Ad_YoutubePlayer vidHeight="1920" vidWidth="1080" />
+      <div className="rounded-xl border-4 border-white my-3 ml-3 overflow-hidden shrink-0 w-2xl">
+        <ReactPlayer url="https://www.youtube.com/watch?v=saXzXq3lXnM" config={{ youtube: { playerVars: { controls: 1 } }}}/>
       </div>
 
-      <div className="grid grid-cols-2 grid-rows-5 gap-1  mx-auto">
+      <div className="grid grid-cols-1 grid-rows-2 gap-1 mx-auto">
         <a
           href="https://www.youtube.com/@PureKoor"
-          className="col-start-1 col-end-2 row-start-1 border-4 bg-red-600 rounded-xl "
+          target="_blank" rel="noopener noreferrer"
+          className="border-4 bg-red-600 rounded-xl flex w-3/4 h-3/4 m-auto"
         >
-          <p className="text-2xl text-center font-bold text-white align-right hover:underline">
-            Subscribe!
+          <p className="text-6xl m-auto font-bold align-middle text-white align-right hover:underline flex">
+            <FaYoutube className="mr-3"/>Subscribe!
           </p>
         </a>
-
-        <div className="row-start-3 row-span-3 col-span-full ml-10">
-          <ul className="text-xl font-regular font-serif text-white text-stroke-sm">
-            <li>- Art Tutorials</li>
-            <li>- Furry Fandom Analysis</li>
-            <li>- Video Game Discussions</li>
-            <li>- Stuff I find interesting</li>
-          </ul>
-        </div>
+        <Func_AutoScrollThumbnails />
       </div>
-      <Func_AutoScrollThumbnails />
     </div>
   );
 }
