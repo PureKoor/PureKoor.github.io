f463bfa926e914bbe6fcd7eb17604cc908e6cf59
Zasz <44956770+bluezasz@users.noreply.github.com>
2025-05-27 00:26:05 -0700

    Center Somna deal and run linter

--- koor-site/src/components/Boxes/Main_SomnaDeal.js
+++ koor-site/src/components/Boxes/Main_SomnaDeal.js
@@ -2,56 +2,55 @@ import { FaPatreon } from "react-icons/fa6";
 import { GiPawPrint } from "react-icons/gi";
 
 
-function Main_SomnaDeal () {
-    return (
-      <>
- {/* "text-2xl [writing-mode:vertical-lr] mt-3" ,  */}
+function Main_SomnaDeal() {
+  return (
+    <>
+      {/* "text-2xl [writing-mode:vertical-lr] mt-3" ,  */}
 
-                <div className="bg-[#ffb44a] min-h-[17rem] overflow-visible">
-            <div className="flex flex-row justify-between gap-5 flex-wrap lg:flex-nowrap items-center mr-3">
-
-<div className="grow shrink-0 grid grid-cols-5 grid-rows-2 gap-3 mt-3">
-                <div className="col-span-full row-span-1">
-                   <div className="text-white font-serif text-center">
-                      <span className="text-6xl align-middle text-stroke-sm">
-                         <a href="https://furality.online/explore/dealers?id=831" alt="Furality Booth #831">
-                        SITE UNDER CONSTRUCTION
-                         </a>
-                      </span>
-                    <div className="row-span-3 col-span-3 mx-auto">
-                      <span className="text-3xl text-center font-serif font-extrabold underline text-white text-stroke-sm">
-                        COME BACK DURING FURALITY SOMNA: Jun 5th - 8th
-                      </span>
-                    </div>
-                    <div className="row-span-3 col-span-3 mx-auto">
-                      <span className="text-2xl text-center font-arial text-white text-stroke-sm">
-                        Early Somna Booth Can Be Found At The 2D Illustrations - Middle Pavilion -#831
-                      </span>
-                    </div>
-                      <div className="row-span-3 col-span-3 mx-auto">
-                      <span className="text-2xl text-center font-arial text-blue text-stroke-sm">
-                        <a href="https://furality.online/explore/dealers?id=831" alt="Furality Booth #831">
-                        BOOKMARK MY BOOTH VIA THE FURALITY DEALER'S DEN EXPLORER
-                        </a>
-                      </span>
-                    </div>
-                  </div>
+      <div className="bg-[#ffb600] overflow-visible">
+        <div className="flex flex-row justify-between gap-5 flex-wrap lg:flex-nowrap items-center mr-3">
+          <div className="grow shrink-0 grid grid-cols-5 grid-rows-1 gap-3 mt-3 ">
+            <div className="col-span-full row-span-1">
+              <div className="text-white font-serif text-center">
+                <span className="text-6xl align-middle text-stroke-sm">
+                  <a href="https://furality.online/explore/dealers?id=831" alt="Furality Booth #831">
+                    SITE UNDER CONSTRUCTION
+                  </a>
+                </span>
+                <div className="row-span-3 col-span-3 mx-auto">
+                  <span className="text-3xl text-center font-serif font-extrabold underline text-white text-stroke-sm">
+                    COME BACK DURING FURALITY SOMNA: Jun 5th - 8th
+                  </span>
+                </div>
+                <div className="row-span-3 col-span-3 mx-auto">
+                  <span className="text-2xl text-center font-arial text-white text-stroke-sm">
+                    Early Somna Booth Can Be Found At The 2D Illustrations - Middle Pavilion -#831
+                  </span>
+                </div>
+                <div className="row-span-3 col-span-3 mx-auto">
+                  <span className="text-2xl text-center font-arial text-blue text-stroke-sm">
+                    <a href="https://furality.online/explore/dealers?id=831" alt="Furality Booth #831">
+                      BOOKMARK MY BOOTH VIA THE FURALITY DEALER'S DEN EXPLORER
+                    </a>
+                  </span>
                 </div>
               </div>
-             <div className="md:m-auto mx-auto mb-3 grow-0 shrink-0 rounded-2xl border-4 bg-indigo-950 border-indigo-950 ">
-                <a
-                  href="https://furality.online/explore/dealers?id=831"
-                  className="h-32 w-32 flex mb-"
-                >
-                  <GiPawPrint color="white" className="m-auto h-full w-2/3 object-cover"/>
-                </a>
-              </div>
-
-
-                          </div>
+            </div>
           </div>
-              </>
-    );
+          <div className="md:m-auto mx-auto mb-3 grow-0 shrink-0 rounded-2xl border-4 bg-indigo-950 border-indigo-950 ">
+            <a
+              href="https://furality.online/explore/dealers?id=831"
+              className="h-32 w-32 flex mb-"
+            >
+              <GiPawPrint color="white" className="m-auto h-full w-2/3 object-cover" />
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Main_SomnaDeal;
