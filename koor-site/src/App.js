fbc18dd6b63482991806868244691cfbd00a088c
Zasz <44956770+bluezasz@users.noreply.github.com>
2025-05-27 00:49:26 -0700

    Fix metadata

--- koor-site/src/App.js
+++ koor-site/src/App.js
@@ -16,14 +16,6 @@ import Main_Tiktok from "./components/Boxes/Main_Tiktok";
 export default function App() {
   return (
     <>
-      <KoorMetaTags
-        title="PureKoor"
-        description="Pure Tycoon creating artwork, videos, and more!"
-        image="https://purekoor.com/assets/Meta/SocialPreview.png"
-        name="furry,art,artwork,freelance,purekoor,streaming,video,creator,digital,3D,Avatar"
-        url="https://purekoor.com"
-        author="PureKoor"
-      />
       <div className="h-fit bg-gradient-to-b from-[#110067] to-[#540051] overflow-hidden">
         <Main_CoverImage />
         <div className="flex flex-col flex-start items-center justify-center mx-auto max-w-screen-xl gap-2">
