befcb3d7461e5313e708724ade9c7e128def3a5f
Zasz <44956770+bluezasz@users.noreply.github.com>
2025-06-06 02:30:16 -0700

    Try to fix LCP

--- koor-site/src/components/Boxes/Main_CoverImage.js
+++ koor-site/src/components/Boxes/Main_CoverImage.js
@@ -4,6 +4,7 @@ function Main_CoverImage() {
   return <LazyImage
   src="assets/Somna/Logo_PureKoorSomna.webp"
   placeholder="assets/Somna/Logo_PureKoorSomnaLQ.webp"
+  fetchpriority="high"
   alt="Nice image"
   className="max-w-7xl h-full"
 />
