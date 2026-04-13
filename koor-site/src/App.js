5e99757f170bb0bde95a0846a3529a7b2a936486
PureKoor <38826280+PureKoor@users.noreply.github.com>
2026-04-12 23:15:26 -0700

    Changes were made

--- koor-site/src/App.js
+++ koor-site/src/App.js
@@ -1,15 +1,16 @@
-import KoorMetaTags from "./components/KoorMetaTags";
-import KoorParticles from "./components/KoorParticles"
-
 import Main_CoverImage from "./components/Boxes/Main_CoverImage";
 import Main_NavBar from "./components/Boxes/Main_NavBar";
-import Main_SomnaDeal from "./components/Boxes/Main_SomnaDeal";
+import Main_AFC2025 from "./components/Boxes/Main_AFC2025";
 import Main_TFYourSona from "./components/Boxes/Main_TFYourSona";
 import Main_Youtube from "./components/Boxes/Main_Youtube";
 import Main_Tanuki from "./components/Boxes/Main_Tanuki";
 import Main_Artwork from "./components/Boxes/Main_Artwork";
 import Main_FootBar from "./components/Boxes/Main_FootBar";
 import Main_Patreon from "./components/Boxes/Main_Patreon";
+
+/* Old Or Unused */
+import KoorMetaTags from "./components/KoorMetaTags";
+import Main_SomnaDeal from "./components/Boxes/Main_SomnaDeal";
 import Main_Tiktok from "./components/Boxes/Main_Tiktok";
 
 {
@@ -18,26 +19,20 @@ import Main_Tiktok from "./components/Boxes/Main_Tiktok";
 export default function App() {
   return (
     <>
-  {/* bg-gradient-to-b from-[#110067] to-[#540051]  */}
       <div className="h-fit bg-gradient-to-b from-[#110067] to-[#540051] overflow-hidden">
-        <div className="z-10">
-          <KoorParticles />
-        </div>
-        <div className="z-20 relative">
-          <div className="flex flex-col items-center"><Main_CoverImage /></div>
-          <div className="flex flex-col flex-start items-center justify-center mx-auto max-w-screen-xl gap-2">
-            <div className="max-w-screen-xl w-full">
-              <Main_NavBar />
-              <Main_SomnaDeal />
-            </div>
-            <Main_Youtube />
-            <Main_Patreon />
-            <Main_TFYourSona />
-            <Main_Artwork />
-            {/* <Main_Tiktok /> */}
-            <Main_Tanuki />
-            <Main_FootBar />
+        <Main_CoverImage />
+        <div className="flex flex-col flex-start items-center justify-center mx-auto max-w-screen-xl gap-2">
+          <div className="max-w-screen-xl w-full">
+            <Main_NavBar />
+            <Main_AFC2025 />
           </div>
+          <Main_Youtube />
+          <Main_Patreon />
+          <Main_TFYourSona />
+          <Main_Artwork />
+          {/* <Main_Tiktok /> */}
+          <Main_Tanuki />
+          <Main_FootBar />
         </div>
       </div>
     </>
---
+++ koor-site/src/components/Boxes/Main_AFC2025.js
@@ -0,0 +1,107 @@
+import { FaPatreon } from "react-icons/fa6";
+import { GiPawPrint } from "react-icons/gi";
+
+function Main_SomnaDeal() {
+  return (
+    <>
+      {/* "text-2xl [writing-mode:vertical-lr] mt-3" ,  */}
+
+      <div className="bg-[#ffb600] overflow-visible">
+        <div className="flex flex-row justify-between gap-5 flex-wrap lg:flex-nowrap items-center mr-3">
+          <div className="grow shrink-0 grid grid-cols-5 grid-rows-1 gap-3 mt-3 ">
+            <div className="col-span-full row-span-1">
+              <div className="text-white font-serif text-center">
+                <div className="transition-transform hover:scale-x-110 hover:-translate-y-2 hover:border-[#FF00FF] duration-450 ease-in-out pb-1">
+                  <span className="text-4xl align-middle text-stroke-sm ">
+                    <a
+                      href="https://furality.online/explore/dealers?id=831"
+                      alt="Furality Booth #831"
+                    >
+                      Find Me @ AFC 2025
+                    </a>
+                  </span>
+                </div>
+                <div className="row-span-3 col-span-3 mx-auto pb-5">
+                  <span className="text-2xl text-center font-arial text-blue text-stroke-sm">
+                    <a
+                      href="https://furality.online/explore/dealers?id=831"
+                      alt="Furality Booth #831"
+                    ></a>
+                  </span>
+                </div>
+
+                <div
+                  className="row-span-3 col-span-3 mx-auto
+                transition-transform hover:scale-x-110 hover:-translate-y-2 hover:border-[#FF00FF] duration-450 ease-in-out"
+                >
+                  <span className="text-4xl text-center font-arial text-blue text-stroke-sm">
+                    <a
+                      href="https://furality.online/event?id=community-showcase"
+                      alt="Furality Somna Community Showcase"
+                    >
+                      Creating Your Artstyle: Where To Begin?
+                    </a>
+                  </span>
+                </div>
+                <div className="row-span-3 col-span-3 mx-auto pb-5">
+                  <span className="text-2xl text-center font-arial text-blue text-stroke-sm">
+                    <a
+                      href="https://furality.online/event?id=community-showcase"
+                      alt="Furality Somna Community Showcase"
+                    >
+                      Friday Sept 19th, Begins 2PM @ on Second Floor in Panel
+                      Room 1
+                    </a>
+                  </span>
+                </div>
+
+                <div
+                  className="row-span-3 col-span-3 mx-auto
+                transition-transform hover:scale-x-110 hover:-translate-y-2 hover:border-[#FF00FF] duration-450 ease-in-out"
+                >
+                  <span className="text-4xl text-center font-arial text-blue text-stroke-sm">
+                    <a
+                      href="https://furality.online/event?id=lobby"
+                      alt="Furality Somna Lobby"
+                    >
+                      TF Meet N' Greet
+                    </a>
+                  </span>
+                </div>
+                <div className="row-span-3 col-span-3 mx-auto pb-6">
+                  <span className="text-2xl text-center font-arial text-blue text-stroke-sm">
+                    <a
+                      href="https://furality.online/event?id=lobby"
+                      alt="Furality Somna Lobby"
+                    >
+                      Saturday Sept 20th, From 9PM - 10PM on First Floor in
+                      Meetup Room 1
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            className="m-auto mb-20 grow-0 shrink-0 rounded-2xl border-4 mr-7 -ml-7
+          transition-transform hover:scale-y-110 hover:-translate-y-4 overflow-hidden duration-450 ease-in-out"
+          >
+            <a
+              href="https://furality.online/explore/dealers?id=831"
+              className="h-50 w-80 flex"
+            >
+              <img
+                src="/assets/Somna/img_Booth831.webp"
+                alt="Furality Somna June 5th through the 8th"
+                width="453"
+                height="801"
+              ></img>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main_SomnaDeal;
