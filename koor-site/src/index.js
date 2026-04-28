74272e3c05b25a49a1a02e8b053437a240c5b326
Zasz <44956770+bluezasz@users.noreply.github.com>
2026-04-28 01:37:02 -0700

    Remove scroll pause for now

--- koor-site/src/index.js
+++ koor-site/src/index.js
@@ -4,10 +4,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-if ("scrollRestoration" in window.history) {
-  window.history.scrollRestoration = "manual";
-}
-window.scrollTo(0, 0);
+// if ("scrollRestoration" in window.history) {
+//   window.history.scrollRestoration = "manual";
+// }
+// window.scrollTo(0, 0);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
