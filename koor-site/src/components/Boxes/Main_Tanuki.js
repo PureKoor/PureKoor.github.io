644b77fe861874b7eed7b1da7c7c83b9910be1eb
Zasz <44956770+bluezasz@users.noreply.github.com>
2026-05-05 01:55:10 -0700

    Fix tanuki fella on mobile

--- koor-site/src/components/Boxes/Main_Tanuki.js
+++ koor-site/src/components/Boxes/Main_Tanuki.js
@@ -8,47 +8,39 @@ function Main_Tanuki() {
     <>
       <div id="avatar" className="bg-[#ffb600] w-full">
         <div
-          className="flex flex-row justify-between gap-5 flex-wrap lg:flex-nowrap items-center
+          className="flex flex-row justify-between gap-5 flex-wrap items-center
         bg-[url('../public/assets/bg_box/pexels-eberhardgross-1367192.webp')] max-w-screen-xl w-full bg-cover bg-bottom bg-no-repeat"
         >
-          <img
-            loading="lazy"
-            alt="Spinning tanuki animation"
-            className="h-72
-              mx-auto md:mx-3 md:mt-0 mt-3 transition-all brightness-95 hover:brightness-110 hover:scale-110 select-none duration-750 ease-in-out
-            active:scale-x-[-1] "
-            src="assets/TanukiFellaSpin.webp"
-          />
+          <div className="w-full md:w-auto md:mx-3 my-3 flex mx-auto justify-center">
+            <img
+              loading="lazy"
+              alt="Spinning tanuki animation"
+              className="h-72 transition-all brightness-95 hover:brightness-110 hover:scale-110 select-none duration-750 ease-in-out active:scale-x-[-1]"
+              src="assets/TanukiFellaSpin.webp"
+            />
+          </div>
 
-          <div className="grow min-w-0 grid grid-cols-5 grid-rows-3 h-full gap-3 mt-5">
-            <div className="col-span-full row-span-1 my-auto">
-              <div className="text-white font-serif text-center">
-                <p className="text-3xl lg:text-6xl align-middle text-stroke-sm hover:underline transition-transform hover:scale-110 hover:-translate-y-2 duration-50 ease-in">
-                  <a
-                    href="https://www.patreon.com/purekoor/shop/tanuki-fella-avatar-support-ver-1-1-229840"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    TANUKI FELLA
-                  </a>
-                </p>
-                <p className="text-3xl align-middle text-stroke-sm">
-                  My First Ever Avatar
-                </p>
-              </div>
+          <div className="grow min-w-0 flex flex-col justify-center gap-3 my-3">
+            <div className="text-white font-serif text-center">
+              <p className="text-3xl lg:text-6xl align-middle text-stroke-sm hover:underline transition-transform hover:scale-110 hover:-translate-y-2 duration-50 ease-in">
+                <a
+                  href="https://www.patreon.com/purekoor/shop/tanuki-fella-avatar-support-ver-1-1-229840"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  TANUKI FELLA
+                </a>
+              </p>
+              <p className="text-3xl align-middle text-stroke-sm">
+                My First Ever Avatar
+              </p>
             </div>
-            {/* <div className="col-start-3 row-start-2 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div>
-              <div className="col-start-1 row-start-2 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div>
-              <div className="col-start-2 row-start-2 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div>
-              <div className="col-start-1 row-start-3 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div>
-              <div className="col-start-2 row-start-3 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div>
-              <div className="col-start-3 row-start-3 rounded-full h-24 w-24 border-4 mx-auto border-[#4c1a1e]"></div> */}
 
-            <div className="row-span-2 col-span-full h-32 my-auto">
+            <div>
               <p className="text-2xl text-center font-extrabold text-white text-stroke-sm">
                 Made for VRChat
               </p>
-              <p className="text-2xl text-center font-extrabold text-white text-stroke-sm pb-3">
+              <p className="text-2xl text-center font-extrabold text-white text-stroke-sm">
                 Quest & PC Compatible
               </p>
             </div>
